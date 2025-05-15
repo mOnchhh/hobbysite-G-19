87e7ca42cdc455b5786a6c07863e9a7ea373e8bc
mOnchhh <ryle.rodriguez@student.ateneo.edu>
2025-05-15 20:04:35 +0800

    Update views.py

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Post, PostCategory
+class PostListView(ListView):
+    model = Post
+    template_name = "post_page.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post.html"
+
+def post_page(request):
+    posts = Post.objects.all()
+    ctx = { "posts": posts }
+    return render(request, 'post_page.html', ctx)
+
+def post(request, pk):
+    ctx = { "post":Post.objects.get(pk=pk) }
+    return render(request, 'post.html', ctx)
