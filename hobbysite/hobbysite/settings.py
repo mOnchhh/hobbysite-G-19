1d66a740a701ecfb390bdb1cd9f674b33b87ca23
mOnchhh <ryle.rodriguez@student.ateneo.edu>
2025-05-21 02:55:06 +0800

    Fonts

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wiki',
 ]
 
 MIDDLEWARE = [
@@ -115,9 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
+
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/wiki/articles'
+LOGOUT_REDIRECT_URL = '/wiki/articles'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # path('', include('merchstore.urls', namespace="merchstore")),
+    # path('', include('commissions.urls', namespace="commissions")),
+    path('', include('wiki.urls', namespace="wiki")),
+    path('user/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    # path('', include('blog.urls', namespace = 'blog')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
